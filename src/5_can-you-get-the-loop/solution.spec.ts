7399a93389772495f17b56910a110eb3393ab33b
Max-Richard Barz <max-richard.barz@siemens.com>
2022-10-21 13:32:32 +0200

    5: can you get the loop

--- src/5_can-you-get-the-loop/solution.spec.ts
+++ src/5_can-you-get-the-loop/solution.spec.ts
@@ -4,13 +4,13 @@ describe('sample tests', function () {
   beforeEach(() => (Node.counter = 0));
 
   it.each([
-    ['A=>A', 1],
-    ['A=>B=>A', 2],
-    ['A=>B=>B', 1],
-    ['A=>B=>C=>C', 1],
-    ['A=>B=>C=>B', 2],
-    ['A=>B=>C=>A', 3],
-    ['A=>B=>C=>1=>2=>3=>4=>5=>6=>7=>8=>9=>10=>11=>12=>1', 12],
+    ['A => A', 1],
+    ['A => B => A', 2],
+    ['A => B => B', 1],
+    ['A => B => C => C', 1],
+    ['A => B => C => B', 2],
+    ['A => B => C => A', 3],
+    ['A B C 1 2 3 4 5 6 7 8 9 10 11 12 1', 12],
   ])('should work for %s', (s, n) => {
     expect(loop_size(build(s))).toEqual(n);
   });
@@ -18,7 +18,7 @@ describe('sample tests', function () {
 
 function build(s: string): Node {
   const map: Record<string, Node> = {};
-  const nodes = s.split('=>');
+  const nodes = s.split(/\s*=>\s*|\s*,\s*|\s+/);
   nodes.forEach((n) => {
     if (!map[n]) map[n] = new Node();
   });
